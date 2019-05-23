7ee1e3c6df7936252a631a79011557502a9b8be5
Ysb <15810856489@163.com>
2019-05-23 17:27:59 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require("path");
+const checkPlugin = require("awesome-typescript-loader").checkPlugin;
 module.exports = {
     entry: "src/index.js",
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    plugins:[
+        new checkPlugin()
+    ]
 }
