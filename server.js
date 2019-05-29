11609b6e50141589720a17e315383ce853dce707
Ysb <15810856489@163.com>
2019-05-29 10:31:43 +0800

    修改 server 文件

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const webpack = require("webpack");
-const option = require("./webpack.config.dev");
+const option = require("./webpack/webpack.config.dev");
 const DevServer = require("webpack-dev-server");
 
 
 const compier = webpack(option);
 const listen = new DevServer(compier);
 listen.listen(8000, () => { console.log("start...") })
+listen.close(() => {console.log("server is closing...")})
